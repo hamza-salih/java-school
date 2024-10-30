e7223b86c484894b13b98d890323a9a4762d6220
Hamza Salih <96381046+hamza-salih@users.noreply.github.com>
2024-10-30 12:54:12 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,24 +3,24 @@ package com.main;
 import java.sql.*;
 
 public class Main {
-    private final static String url = "jdbc:postgresql://localhost:5432/javaDB";
+    private final static String url = "jdbc:postgresql://localhost:5432/games";
     private final static String driverName = "org.postgresql.Driver";
     private final static String username = "postgres";
-    private final static String password = "nidal";
+    private final static String password = "hamza";
 
     public static void main(String[] args) {
         try {
             Class.forName(driverName);
             Connection conn = DriverManager.getConnection(url, username, password);
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM langs");
+            ResultSet rs = st.executeQuery("SELECT * FROM games");
             while (rs.next()) {
                 System.out.println(rs.getString(1) + " - " + rs.getString(2));
             }
             rs.close();
             st.close();
         } catch (Exception e) {
-            System.out.println("======\nError : \n" + e.getMessage() + "\n======");
+            System.out.println("==\nError : \n" + e.getMessage() + "\n===");
         }
     }
 }
