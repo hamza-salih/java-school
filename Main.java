45ff0f33275b479b185234caf2708e3dce686e9e
Hamza Salih <96381046+hamza-salih@users.noreply.github.com>
2024-10-30 12:53:03 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package com.main;
+
+import java.sql.*;
+
+public class Main {
+    private final static String url = "jdbc:postgresql://localhost:5432/javaDB";
+    private final static String driverName = "org.postgresql.Driver";
+    private final static String username = "postgres";
+    private final static String password = "nidal";
+
+    public static void main(String[] args) {
+        try {
+            Class.forName(driverName);
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT * FROM langs");
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " - " + rs.getString(2));
+            }
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            System.out.println("======\nError : \n" + e.getMessage() + "\n======");
+        }
+    }
+}
